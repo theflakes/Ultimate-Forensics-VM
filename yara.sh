bc45264165dac33331a93037219989efcfac263a
theflakes <theflakes@gmail.com>
2016-09-18 23:44:08 -0400

    Update yara.sh

--- yara.sh
+++ yara.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-for file in $(find $1 -maxdepth 1 -type f -name "*.yar"); do 
-	if zenity --question --text="Use $file?" --ok-label=Yes --cancel-label=No
-	then
-    		yara -w $file $2
-	fi
-done
+if [ $# -ne 2 ]
+then
+        echo "Usage: $0 /path/to/yara/rules file_to_scan"
+else
+        for file in $(find $1 -maxdepth 1 -type f -name "*.yar"); do
+                if zenity --question --text="Use $file?" --ok-label=Yes --cancel-label=No
+                then
+                        yara -w $file $2
+                fi
+        done
+fi
