16383ad861b54459512a69094f47e872c37aecf4
theflakes <theflakes@gmail.com>
2017-03-15 15:27:46 -0400

    Update start-sonion.sh

--- docker/start-sonion.sh
+++ docker/start-sonion.sh
@@ -3,4 +3,18 @@
 # start SecurityOnion docker and allow GUI applications to display
 
 xhost +local:docker
-docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix sonion
+docker run -it --rm -e DISPLAY=$DISPLAY \
+	--privileged \
+	-v /tmp/.X11-unix/:/tmp/.X11-unix \
+	-v /etc/localtime:/etc/localtime:ro \
+        -v $HOME/Downloads:/root/Downloads \
+        -v $HOME/Pictures:/root/Pictures \
+        -v $HOME/Torrents:/root/Torrents \
+        -v $HOME/.chrome:/data \
+        -v /dev/shm:/dev/shm \
+        -v /etc/hosts:/etc/hosts \
+        --device /dev/snd \
+        --device /dev/dri \
+        --group-add audio \
+        --group-add video \
+	      sonion 
